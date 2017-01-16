7b712a697850a2b02841feeab99c415367ecdf6b
LordGee <gordon.johnson@mychaos.co.uk>
2017-01-16 14:53:51 +0000

    SQL Build File
    Added remaining tables.
    Need to double check these with ERD.
    To be tested on a localhost
    
    - TODO add the relationships

--- Private/DB_Build/mysql_create_tables.sql
+++ Private/DB_Build/mysql_create_tables.sql
@@ -39,3 +39,65 @@ CREATE TABLE stock_hold (
   stock_hold_id INTEGER PRIMARY KEY AUTO-INCREMENT,
   quantity INTEGER NOT NULL DEFAULT = 0,
 );
+
+CREATE TABLE campaign (
+  campaign_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  offer_name VARCHAR (255)
+);
+
+CREATE TABLE campaign_line (
+  campaign_line_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  start_date DATE NOT NULL,
+  finish_date DATE NOT NULL
+);
+
+CREATE TABLE category (
+  category_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  wine_colour VARCHAR (25),
+  wine_type VARCHAR (25)
+);
+
+CREATE TABLE customer_order (
+  customer_order_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  order_date DATE NOT NULL,
+  total_value DOUBLE (10, 2),
+);
+
+CREATE TABLE customer_order_line (
+  customer_order_line_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  line_value DOUBLE (10, 2) NOT NULL
+);
+
+CREATE TABLE payment (
+  payment_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  card_type VARCHAR (50) NOT NULL,
+  card_name VARCHAR (100) NOT NULL,
+  card_number VARCHAR (16) NOT NULL,
+  expiry_date DATE NOT NULL
+);
+
+-- Web Management
+
+CREATE TABLE admin (
+  admin_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  first_name VARCHAR (100),
+  last_name VARCHAR (100) NOT NULL,
+  email_address VARCHAR (255) NOT NULL UNIQUE,
+  password VARCHAR (255) NOT NULL,
+  access_level INTEGER NOT NULL
+);
+
+CREATE TABLE navigation (
+  navigation_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  page_name VARCHAR (255) NOT NULL,
+  url_link VARCHAR (255) NOT NULL,
+  access_level INTEGER NOT NULL DEFAULT = 10
+);
+
+CREATE TABLE static_content (
+  static_content_id INTEGER PRIMARY KEY AUTO-INCREMENT,
+  subject_heading VARCHAR (255),
+  subject_content CLOB NOT NULL,
+  paragraph INTEGER NOT NULL DEFAULT = 1,
+  image_link VARCHAR (255),
+);
