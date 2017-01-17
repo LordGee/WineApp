d1d51ad1dcfcb092b1f176f9fe0451c266452c1c
LordGee <gordon.johnson@mychaos.co.uk>
2017-01-17 15:01:22 +0000

    Headers and footers
    Started to add front page, including header area, navigation and footer.

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Ten Green Bottles</title>
+    <title>Ten Green Bottles</title>
+    <link rel="stylesheet" href="Style/main.css" type="text/css" />
 </head>
 <body>
-<h1>Ten Green Bottles</h1>
+    <div class="mainArea">
+        <header class="topArea">
+            <h1 id="headTitle">Ten Green Bottles</h1>
+        </header>
+        <nav class="navArea">
+            <ul id="navUl">
+                <li id="navLi"><a href="index.php">Home</a></li>
+                <li id="navLi"><a href="about.php">About Us</a></li>
+                <li id="navLi"><a href="wine.php">Wine</a></li>
+            </ul>
+        </nav>
+        <div class="contentArea">
+            
+        </div>
+        <footer class="bottomArea">
+        
+        </footer>
+    </div>
 </body>
 </html>
