8d8e16b9555ab43503fff15b39bc4ead6d97577a
LordGee <gordon.johnson@mychaos.co.uk>
2017-02-05 23:41:29 +0000

    Database Access
    + Made some amendments to the database access file
    + Added the first Get All prepared requirest function to read and
    connect the database

--- database/db_access.php
+++ database/db_access.php
@@ -4,10 +4,17 @@
     error_reporting(E_ALL);
 
     $db_host = "localhost";
-    $db_user = "wine";
+    $db_user = "wine_user";
     $db_name = "wine_app";
-    $db_pass = "paul";
+    $db_pass = "paulNeve";
 
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name",$db_user,$db_pass);
 
+    function getAllWines() {
+        global $pdo;
+        $statement = $pdo->prepare('SELECT * FROM wine');
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_CLASS, 'Wine');
+        return $result;
+    }
 ?>
