148509e6b953818da39451c05342e2cddb8f0255
LordGee <gordon.johnson@mychaos.co.uk>
2017-02-06 09:21:56 +0000

    Minor Changes

--- Controllers/wine_controller.php
+++ Controllers/wine_controller.php
@@ -1,8 +1,8 @@
 <?php
 
-    require_once("/Database/db_access.php");
-    require_once ("/Model/m_wine.php");
-    require_once ("/Includes/session.php");
+    require_once("Database/db_access.php");
+    require_once ("Model/m_wine.php");
+    require_once ("Includes/session.php");
 
     $accessWines = [];
     $accessWines = getAllWines();
