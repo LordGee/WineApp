44d1a42519af23750f221ab6960239959edea5dd
saisarwar <saira_sarwar11@hotmail.co.uk>
2017-02-06 12:43:24 +0000

    navigation bar
    - pushed navigation bar to top
    - changed background colour

--- Includes/header.php
+++ Includes/header.php
@@ -6,18 +6,18 @@
 </head>
 <body>
 <div class="mainArea">
+  <div class="navArea">
+      <nav >
+          <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="about.php">About Us</a></li>
+              <li><a href="wine.php">All Wines</a></li>
+          </ul>
+      </nav>
+  </div>
     <header class="topArea">
         <div id="logo">
-            <img src="image/wines.png" width="100%" height="200px;"/>
+            <img src="image/10GreenBottles.png"/>
         </div>
     </header>
-    <div class="navArea">
-        <nav >
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About Us</a></li>
-                <li><a href="wine.php">All Wines</a></li>
-            </ul>
-        </nav>
-    </div>
     <div id="contentArea">
