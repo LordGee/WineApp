52c215aeba9b3a2ee3e85d448ac17fdd7d62e7fa
saisarwar <saira_sarwar11@hotmail.co.uk>
2017-02-13 13:47:35 +0000

    Replaced Missing code

--- sign_up.php
+++ sign_up.php
@@ -1,28 +1,38 @@
+<?php require_once ("Controllers/customer_controller.php"); ?>
 <?php require_once ("Includes/header.php"); ?>
 
-<h1>Register for a new account today and recieve a 25% discount code on your first order</h1>
-  <div class = "signup">
-<form method="post" action="sign_up_controller">
-    <label>First Name : </label>
-    <input name="first_name"/>
-    <br/>
-    <label>Last Name : </label>
-    <input name="last_name"/>
-    <br/>
-    <label>Emal Address : </label>
-    <input type="email" name="email_address"/>
-    <br/>
-    <label>Phone Number : </label>
-    <input name="phone_number"/>
-    <br/>
-    <label>Door Number : </label>
-    <input name="door_number"/>
-    <br/>
-    <label>Post Code : </label>
-    <input name="post_code"/>
-    <br/>
-    <input type="submit" value="Register Now"/>
-</form>
-</div>
+<h1>Register for a new account today and receive a 25% discount code on your first order</h1>
+<form method="post" action="sign_up.php">
+    <div>
+        <label for="first_name">First Name : </label>
+        <input type="text" name="first_name" id="first_name"  />
+    </div>
+    <div>
+        <label for="last_name">Last Name : </label>
+        <input type="text" name="last_name" id="last_name"  />
+    </div>
+    <div>
+        <label for="email_address">Email Address : </label>
+        <input type="email" name="email_address" id="email_address"  />
+    </div>
+    <div>
+        <label for="password">Password : </label>
+        <input type="password" name="password" id="password"  />
+    </div>
+    <div>
+        <label for="phone_number">Phone Number : </label>
+        <input type="text" name="phone_number" id="phone_number"  />
+    </div>
+    <div>
+        <label for="door_number_name">Door Number : </label>
+        <input type="text" name="door_number_name" id="door_number_name" required />
+    </div>
+    <div>
+        <label for="post_code">Post Code : </label>
+        <input type="text" name="post_code" id="post_code" required />
+    </div>
+    <div>
+    <input type="hidden" name="iCode" value="register">
+    <input type="submit" value="Register Now">
 
 <?php require_once ("Includes/footer.php"); ?>
