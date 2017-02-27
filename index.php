929f57c6e1db5dcc2e6f3e2b94c7edbd4104ab68
saisarwar <saira_sarwar11@hotmail.co.uk>
2017-02-27 13:04:07 +0000

    added description

--- index.php
+++ index.php
@@ -12,7 +12,13 @@
             <h1>H1 Tag</h1>
             <h2>H2 Tag</h2>
             <h3>H3 Tag</h3>
-            <p>Paragraph tag</p>
+            <p>Welcome to Ten Green Bottles, the UK's largest Internet retailer of top quality Non-alcoholic Wines & Champagne for anniversaries, birthdays, corporate gifts and other special occasions. 
+
+We deliver direct, next day, to residential or business addresses anywhere in the UK. All orders are beautifully packaged, register today to receive a 25% discount code on your first order. Call us today on 020 8…. to discuss any special requirements.
+
+We offer a price promise guarantee & tell you when your gift has been delivered! 
+
+</p>
             <li>List Item Tags</li>
             <a>Ancor Tag</a>
 
