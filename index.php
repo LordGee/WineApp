f6e18952c91c3e37b5a72de9bcd45de09c2c112c
saisarwar <saira_sarwar11@hotmail.co.uk>
2017-02-27 19:07:58 +0000

    - styling the front page - adding images - resizing images - adding special offers

--- index.php
+++ index.php
@@ -9,19 +9,36 @@
 
     <h1>Welcome to Ten Green Bottles</h1>
 
-            <h1>H1 Tag</h1>
-            <h2>H2 Tag</h2>
-            <h3>H3 Tag</h3>
-            <p>Welcome to Ten Green Bottles, the UK's largest Internet retailer of top quality Non-alcoholic Wines & Champagne for anniversaries, birthdays, corporate gifts and other special occasions. 
+<figure>
+  <img src ="image/wine_clash.png"
+      alt="Enjoy a lil sparkle"/>
 
-We deliver direct, next day, to residential or business addresses anywhere in the UK. All orders are beautifully packaged, register today to receive a 25% discount code on your first order. Call us today on 020 8…. to discuss any special requirements.
 
-We offer a price promise guarantee & tell you when your gift has been delivered! 
+<p>Welcome to Ten Green Bottles, the UK's largest Internet retailer of top quality non-alcoholic Wines & Champagnes. We cater for anniversaries, birthdays, corporate gifts and other special occasions. 
 
+We deliver direct, next day, to residential or business addresses anywhere in the UK. All orders are beautifully packaged, register today to receive a 25% discount code on your first order. Call us today on 0845 600 1212 to discuss any special requirements.
+
+We offer a price promise guarantee & tell you when your gift has been delivered! </p>
+</figure>
+<h4>Special Offer Below:</h4>
+<p>
+  <img src="image/wine_day_brands.png" width="300px"
+  alt="Managers Special"
+  title="Managers Special"/>
+  <img src="image/classic-flyout.png"width="300px"
+  alt="10% off"
+  title="10% off"/>
+  <img src="image/wine_glasses.png"width="300px"
+  alt="Grape of the Week"
+  title="Grape of the Week"/>
 </p>
-            <li>List Item Tags</li>
-            <a>Ancor Tag</a>
+</body>
+</html>
 
-            <img src="image/2glass.jpg" alt="Enjoy a little sparkle" title="Click here to explore more."/>
+
+10% off
+Grape of the Week
+
+
 
 <?php require_once ("Includes/footer.php"); ?>
