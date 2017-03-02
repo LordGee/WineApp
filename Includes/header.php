e0714f73072e8dfbb2cbc2b56de26048ed56de00
LordGee <gordon.johnson@mychaos.co.uk>
2017-03-02 10:31:41 +0000

    Forgot the Viewport

--- Includes/header.php
+++ Includes/header.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ten Green Bottles</title>
     <link rel="stylesheet" href="Style/main.css" type="text/css" />
     <link rel="stylesheet" href="Style/element.css" type="text/css" />
