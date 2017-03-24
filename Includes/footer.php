b0ac2454e53c4672a573a3eb8552300fb39fb162
saisarwar <saira_sarwar11@hotmail.co.uk>
2017-03-24 02:13:28 +0000

    Adjusting footer
    - adding about us details
    - adding styling in style page

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,8 +1,9 @@
 
-      </div>
+  <body>
       <div>
         <footer class="bottomArea">
-
+          <h5> About us... </h5>
+          <h3> Unit 1-2 North Point Business Centre, Horton Road, London SWL 8EQ </h3>
         </footer>
       </div>
 
