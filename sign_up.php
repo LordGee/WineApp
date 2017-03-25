c4820a86bc52eb3ca322c6345b4d92a25615daef
saisarwar <saira_sarwar11@hotmail.co.uk>
2017-03-25 15:45:30 +0000

    amendments to login page

--- sign_up.php
+++ sign_up.php
@@ -6,36 +6,37 @@
 
 <h1>Register for a new account today & receive a 25% discount code on your first order!!!</h1>
 <form method="post" action="sign_up.php">
+<div class="boxed">
     <div>
         <label for="first_name">First Name : </label>
-        <input type="text" name="first_name" id="first_name"  />
+        <input type="text" name="first_name" id="first_name"><br/><br/>
     </div>
     <div>
         <label for="last_name">Last Name : </label>
-        <input type="text" name="last_name" id="last_name"  />
+        <input type="text" name="last_name" id="last_name"><br/><br/>
     </div>
     <div>
         <label for="email_address">Email Address : </label>
-        <input type="email" name="email_address" id="email_address"  />
+        <input type="email" name="email_address" id="email_address"><br/><br/>
     </div>
     <div>
         <label for="password">Password : </label>
-        <input type="password" name="password" id="password"  />
+        <input type="password" name="password" id="password"><br/><br/>
     </div>
     <div>
         <label for="phone_number">Phone Number : </label>
-        <input type="text" name="phone_number" id="phone_number"  />
+        <input type="text" name="phone_number" id="phone_number"><br/><br/>
     </div>
     <div>
         <label for="door_number_name">Door Number : </label>
-        <input type="text" name="door_number_name" id="door_number_name" required />
+        <input type="text" name="door_number_name" id="door_number_name" required><br/><br/>
     </div>
     <div>
         <label for="post_code">Post Code : </label>
-        <input type="text" name="post_code" id="post_code" required />
+        <input type="text" name="post_code" id="post_code" required><br/><br/>
     </div>
     <div>
-    <input type="hidden" name="iCode" value="register">
+    <input type="hidden" name="iCode" value="register"><br/><br/>
     <input type="submit" value="Register Now">
-
+  </div>
 <?php require_once ("Includes/footer.php"); ?>
