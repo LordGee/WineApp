9419b2fe13681b280073ebabecaf6663c7205ce5
LordGee <gordon.johnson@mychaos.co.uk>
2017-03-25 11:38:43 +0000

    Fixed Footer

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,4 +1,5 @@
         </div>
+        <div class="buffer"></div>
         <div class="bottomArea">
             <h3> Unit 1-2 North Point Business Centre, Horton Road, London SWL 8EQ </h3>
             <a href="rss.php" title="RSS"><i class="fa fa-rss fa-2x" aria-hidden="true"></i> RSS</a>
