5030824d957b671559048acd87e2f9e08ce45d49
LordGee <gordon.johnson@mychaos.co.uk>
2017-03-26 21:36:27 +0100

    Layout Improvements

--- forgot.php
+++ forgot.php
@@ -2,23 +2,23 @@
 <?php require_once ("Controllers/customer_controller.php"); ?>
 <?php require_once ("Includes/info.php"); ?>
 
-<h1>Request a Password Reset Link</h1>
-<p>Enter your email address and we will send you a password reset link to change your password.</p>
-<span class="error"><?= $error ?></span>
-<span class="message"><?= $message ?></span>
-<form method="post" action="forgot.php">
-    <div>
-        <label>Email Address : </label>
-        <input type="email" name="email" placeholder="Enter email address here" required/>
+<div class="row">
+    <div class="col-offset-lg-2 col-lg-8 col-offset-md-1 col-md-10 col-sm-12 formContent">
+        <h1>Request a Password Reset Link</h1>
+        <p>Enter your email address and we will send you a password reset link to change your password.</p>
+        <span class="error"><?= $error ?></span>
+        <span class="message"><?= $message ?></span>
+        <form method="post" action="forgot.php">
+            <label>Email Address : </label>
+            <input type="email" name="email" placeholder="Enter email address here" required/>
+            <br><br>
+            <input type="hidden" name="iCode" value="forgotten" />
+            <input type="submit" value="Reset" />
+            <br><br>
+            <p>Not got an account? Register <a href="sign_up.php">HERE</a> </p>
+        </form>
     </div>
-    <div>
-        <input type="hidden" name="iCode" value="forgotten" />
-        <input type="submit" value="Reset" />
-    </div>
-    <div>
-        <p>Not got an account? Register <a href="sign_up.php">HERE</a> </p>
-    </div>
-</form>
+</div>
 
 
 <?php require_once ("Includes/footer.php"); ?>
--- sign_up.php
+++ sign_up.php
@@ -3,40 +3,35 @@
 <?php require_once ("Includes/info.php"); ?>
 
 
-
-<h1>Register for a new account today & receive a 25% discount code on your first order!!!</h1>
-<form method="post" action="sign_up.php">
-<div class="boxed">
-    <div>
-        <label for="first_name">First Name : </label>
-        <input type="text" name="first_name" id="first_name"><br/><br/>
+<div class="row">
+    <div class="col-offset-lg-2 col-lg-8 col-offset-md-1 col-md-10 col-sm-12 formContent">
+        <h1>Register for a new account</h1>
+        <p>Get 25% off your first order (Subject to terms and conditions)</p>
+        <form method="post" action="sign_up.php">
+            <label for="first_name">First Name : </label>
+            <input type="text" name="first_name" id="first_name">
+            <br><br>
+            <label for="last_name">Last Name : </label>
+            <input type="text" name="last_name" id="last_name">
+            <br><br>
+            <label for="email_address">Email Address : </label>
+            <input type="email" name="email_address" id="email_address">
+            <br><br>
+            <label for="password">Password : </label>
+            <input type="password" name="password" id="password">
+            <br><br>
+            <label for="phone_number">Phone Number : </label>
+            <input type="text" name="phone_number" id="phone_number">
+            <br><br>
+            <label for="door_number_name">Door Number : </label>
+            <input type="text" name="door_number_name" id="door_number_name" required>
+            <br><br>
+            <label for="post_code">Post Code : </label>
+            <input type="text" name="post_code" id="post_code" required>
+            <br><br>
+            <input type="hidden" name="iCode" value="register">
+            <input type="submit" value="Register Now">
+        </form>
     </div>
-    <div>
-        <label for="last_name">Last Name : </label>
-        <input type="text" name="last_name" id="last_name"><br/><br/>
-    </div>
-    <div>
-        <label for="email_address">Email Address : </label>
-        <input type="email" name="email_address" id="email_address"><br/><br/>
-    </div>
-    <div>
-        <label for="password">Password : </label>
-        <input type="password" name="password" id="password"><br/><br/>
-    </div>
-    <div>
-        <label for="phone_number">Phone Number : </label>
-        <input type="text" name="phone_number" id="phone_number"><br/><br/>
-    </div>
-    <div>
-        <label for="door_number_name">Door Number : </label>
-        <input type="text" name="door_number_name" id="door_number_name" required><br/><br/>
-    </div>
-    <div>
-        <label for="post_code">Post Code : </label>
-        <input type="text" name="post_code" id="post_code" required><br/><br/>
-    </div>
-    <div>
-    <input type="hidden" name="iCode" value="register"><br/><br/>
-    <input type="submit" value="Register Now">
-  </div>
+</div>
 <?php require_once ("Includes/footer.php"); ?>
