be68994beb569f5af95a8d3343e4c20208656a8f
LordGee <gordon.johnson@mychaos.co.uk>
2017-03-27 08:14:43 +0100

    Addressed Conflict on the About Page

--- about.php
+++ about.php
@@ -4,35 +4,6 @@ Include 'accessibility.php';
 
 <?php require_once ("Includes/info.php"); ?>
 
-<<<<<<< HEAD
-
-<h1>Ten Green Bottles</h1>
-<h3>About Us!</h3>
-
-<div class="article">
-  <h2>About TGB</h2>
-  <p>Ten Green Bottles web store is dedicated to wine, spirits and champagne.
-     We offer a full range of fine products for you to choose
-     from. With numerous ways to search our catalogue;
-     we hope you'll find exactly what you are looking for.</p>
-  <p>TGB started trading in the early 2000s. Since, the company has grown its
-    trade to carry an extensive range of fine non-alcohlic wines & popular,
-    pretigious spirits & champagnes from around the world, all of which can be
-    found right here! </p>
-</div>
-
-<div class="article">
-  <h2>Before You Start</h2>
-  <p>There are a few things that you should know. You must be over 18 years of
-    age to buy from us and we only deliver to addresses in United Kingdom.
-    Delivery is FREE, but we have a minimum order value of £....
-    We offer non-alcohlic wine & champagne by the case and spirits, including liqueurs and
-    fortified wines are available in litre/bottle size.</p>
-    <p>Orders placed before 12 noon will be delivered the following day or you
-    can select a day that is convenient to you. However, we do not deliver on
-    Sundays or public holidays. Orders required for a Monday delivery should be
-    placed by noon on the previous Saturday.</p>
-=======
 <div class="row">
     <div class="col-offset-lg-2 col-lg-8 col-offset-md-1 col-md-10 col-sm-12">
         <h1>Ten Green Bottles</h1>
@@ -46,7 +17,7 @@ Include 'accessibility.php';
                      from. With numerous ways to search our catalogue;
                      we hope you'll find exactly what you are looking for.</p>
                   <p>TGB started trading in the early 2000s. Since, the company has grown its
-                    trade to carry an extensive range of fine non-alcohlic wines & popular,
+                    trade to carry an extensive range of fine non-alcoholic wines & popular,
                     pretigious spirits & champagnes from around the world, all of which can be
                     found right here! </p>
                 </div>
@@ -57,7 +28,7 @@ Include 'accessibility.php';
                   <p>There are a few things that you should know. You must be over 18 years of
                     age to buy from us and we only deliver to addresses in United Kingdom.
                     Delivery is FREE, but we have a minimum order value of £....
-                    We offer non-alcohlic wine & champagne by the case and spirits, including liqueurs and
+                    We offer non-alcoholic wine & champagne by the case and spirits, including liqueurs and
                     fortified wines are available in litre/bottle size.</p>
                     <p>Orders placed before 12 noon will be delivered the following day or you
                     can select a day that is convenient to you. However, we do not deliver on
@@ -79,6 +50,6 @@ Include 'accessibility.php';
             </div>
         </div>
     </div>
->>>>>>> origin/master
+
 </div>
 <?php require_once ("Includes/footer.php"); ?>
